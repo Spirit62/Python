4e8aea7596b094be16f1ebb3e0d2a72f2b23df8f
Samriddha Jung Pande <Samriddhajungpande@gmail.com>
2026-07-02 21:28:20 +0545

    Basic theory of file handling

---
+++ filehanding.py
@@ -0,0 +1,14 @@
+"""
+filehandling:
+
+  read mode(r): for reading
+  write mode(w): for writing
+  append(a): updating
+  create(x): file create
+
+
+"""
+
+f=open("newfile.txt","w")
+
+f.write("hello2")
