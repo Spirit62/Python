d2e102b0413c8d64613c7ca6519ec340292ad97c
Samriddha Jung Pande <Samriddhajungpande@gmail.com>
2026-07-27 20:44:10 +0545

    Revised bubble sort

--- BUBBLE_SORT.py
+++ BUBBLE_SORT.py
@@ -1,16 +1,13 @@
-arr = [64, 34, 25, 12, 22, 11, 90]
 
-boundary = len(arr) - 1
-Swap = True
-
-while Swap == True:
-    Swap = False
-    for i in range(0, boundary):
-        if arr[i + 1] < arr[i]:
-            
-            arr[i + 1], arr[i] = arr[i], arr[i + 1]
-            Swap = True
-            
-    boundary -= 1
+arr=[64, 34, 25, 12, 22, 11, 90]
+boundary=len(arr)-1
+swap = True
+while swap==True and boundary>0:
+    swap=False
+    for i in range(boundary):
+        if arr[i]>arr[i+1]:
+            arr[i],arr[i+1]=arr[i+1],arr[i]
+            swap = True
+    boundary-=1
 
 print("Sorted array:", arr)
