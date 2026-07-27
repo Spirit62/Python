f778103c1751989095e5526dc154f1e444204909
Samriddha Jung Pande <Samriddhajungpande@gmail.com>
2026-07-27 20:39:43 +0545

    Revised on binary search

--- BINARY_SEARCH.py
+++ BINARY_SEARCH.py
@@ -2,22 +2,22 @@ sorted_arr = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
 search_value = int(input("Enter a value to search with: "))
 
 FirstIndex = 0
-LastIndex = len(sorted_arr) - 1
+LastIndex = len(sorted_arr)-1
 Found = False
 
-while Found == False and FirstIndex <= LastIndex:
-    
-    MidIndex = (FirstIndex + LastIndex) // 2
-    
-    if search_value == sorted_arr[MidIndex]:
+while Found == False and LastIndex>=FirstIndex:
+    MidIndex = (FirstIndex+LastIndex)//2
+    if search_value==sorted_arr[MidIndex]:
         Found = True
-    elif search_value > sorted_arr[MidIndex]:
-        FirstIndex = MidIndex + 1 
+    elif search_value<sorted_arr[MidIndex]:
+        LastIndex= MidIndex-1
     else:
-        LastIndex = MidIndex - 1   
+        FirstIndex=MidIndex+1
+    
+if Found==False:
+    print("Not in Array")
+elif Found==True:
+    print(f"Found at index {MidIndex}")
 
-if Found == False:
-    print("Not in array")
-else:
-    # MidIndex holds the actual position of the element
-    print(f"The position of the search value is {MidIndex}")
+
+
