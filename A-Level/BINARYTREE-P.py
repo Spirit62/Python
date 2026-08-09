b1ad9ce7e30354e6d9a39480eee8dc89ee7f1664
Samriddha Jung Pande <Samriddhajungpande@gmail.com>
2026-08-09 10:07:03 +0545

    Finished DeleteNode logic for BinaryTree

--- A-Level/BINARYTREE-P.py
+++ A-Level/BINARYTREE-P.py
@@ -18,6 +18,61 @@ def AddChildNode(root,value):
         root[2]=value
         print("Added to right")
         break
+def DeleteNode(root,target,parent=None):
+
+  if root is None:
+    return False
+
+
+  if root[0]==target:
+
+
+    #Case 1: Leaf Nodes
+    if root[1]==None and root[2]==None:
+      if parent==None:
+        return None
+      if parent[1]==root:
+        parent[1]=None
+      else:
+        parent[2]=None
+      return True
+
+    #Case 2: One Child
+
+    if root[1]==None or root[2]==None:
+      child = root[1] if root[1] else root[2]
+      if parent == None:
+        return child
+      if parent[1]==root:
+        parent[1]=child
+      else:
+        parent[2]=child
+      return True
+
+
+    #Case 3: Two children
+
+    sucessor_parent=root
+    sucessor=root[2]
+
+    while sucessor[1]:
+      sucessor_parent=sucessor
+      sucessor=sucessor[1]
+    root[0]=sucessor[0]
+
+    if sucessor_parent==root:
+      root[2]=sucessor[2]
+    else:
+      sucessor_parent[1]=sucessor[2]
+    return True
+
+  
+  elif root[0]> target:
+    return DeleteNode(root[1],target,root)
+  else:
+    return DeleteNode(root[2],target,root)
+
+
 
 def InOrderTraversal(root):
   if root is None:
