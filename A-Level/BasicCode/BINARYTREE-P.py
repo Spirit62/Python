aa10e6b304aa9d020396eb9b5293dc526817564e
Samriddha Jung Pande <Samriddhajungpande@gmail.com>
2026-09-14 17:04:57 +0545

    Binary Search and Binary Tree revision

--- A-Level/BasicCode/BINARYTREE-P.py
+++ A-Level/BasicCode/BINARYTREE-P.py
@@ -1,116 +1,43 @@
 def createTreeNode(value):
-  return [value, None,None]
+  return [value,None,None]
 
-def AddChildNode(root,value):
-  value=createTreeNode(value)
+def AddNode(root, node):
+  node=createTreeNode(node)
   while True:
-    if value[0]< root[0]:
+    if node[0]<root[0]:
       if root[1]:
         root=root[1]
       else:
-        root[1]=value
-        print("Added to left")
+        root[1]=node
+        print("Added node to left of", root)
         break
-    else:
+    elif node[0]>root[0]:
       if root[2]:
         root=root[2]
       else:
-        root[2]=value
-        print("Added to right")
+        root[2]=node
+        print("Added node to right of", root)
         break
-def DeleteNode(root,target,parent=None):
-
-  if root is None:
-    return False
-
-
-  if root[0]==target:
-
-
-    #Case 1: Leaf Nodes
-    if root[1]==None and root[2]==None:
-      if parent==None:
-        return None
-      if parent[1]==root:
-        parent[1]=None
-      else:
-        parent[2]=None
-      return True
-
-    #Case 2: One Child
-
-    if root[1]==None or root[2]==None:
-      child = root[1] if root[1] else root[2]
-      if parent == None:
-        return child
-      if parent[1]==root:
-        parent[1]=child
-      else:
-        parent[2]=child
-      return True
-
-
-    #Case 3: Two children
-
-    sucessor_parent=root
-    sucessor=root[2]
-
-    while sucessor[1]:
-      sucessor_parent=sucessor
-      sucessor=sucessor[1]
-    root[0]=sucessor[0]
-
-    if sucessor_parent==root:
-      root[2]=sucessor[2]
     else:
-      sucessor_parent[1]=sucessor[2]
-    return True
-
-  
-  elif root[0]> target:
-    return DeleteNode(root[1],target,root)
-  else:
-    return DeleteNode(root[2],target,root)
-
-
+      print("Value already exists in the tree")
 
 def InOrderTraversal(root):
   if root is None:
     return
-
   InOrderTraversal(root[1])
   print(root[0])
   InOrderTraversal(root[2])
 
 def PreOrderTraversal(root):
   if root is None:
-      return
+    return
   print(root[0])
   PreOrderTraversal(root[1])
   PreOrderTraversal(root[2])
 
 def PostOrderTraversal(root):
   if root is None:
-      return
+    return
   PostOrderTraversal(root[1])
   PostOrderTraversal(root[2])
   print(root[0])
-root = createTreeNode(10)
-
-# Add elements
-AddChildNode(root, 5)
-AddChildNode(root, 15)
-AddChildNode(root, 3)
-AddChildNode(root, 7)
-
-# Print sorted output
-print("\nIn-Order Traversal:")
-InOrderTraversal(root)
-
-print("\nPre-Order Traversal:")
-PreOrderTraversal(root)
-
-print("\nPost-Order Traversal:")
-PostOrderTraversal(root)
-
-
--- A-Level/BasicCode/BINARY_SEARCH.py
+++ A-Level/BasicCode/BINARY_SEARCH.py
@@ -1,23 +1,20 @@
 sorted_arr = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
-search_value = int(input("Enter a value to search with: "))
+search_value= int(input("Enter the number to search: "))
+LastIndex= len(sorted_arr)-1
+FirstIndex=0
+Found=False
+while Found==False and LastIndex>=FirstIndex:
+  MidIndex=(FirstIndex+LastIndex)//2
+  if search_value==sorted_arr[MidIndex]:
+    Found=True
+  elif search_value<sorted_arr[MidIndex]:
+    LastIndex=MidIndex-1
+  else:
+    FirstIndex=MidIndex+1
 
-FirstIndex = 0
-LastIndex = len(sorted_arr)-1
-Found = False
-
-while Found == False and LastIndex>=FirstIndex:
-    MidIndex = (FirstIndex+LastIndex)//2
-    if search_value==sorted_arr[MidIndex]:
-        Found = True
-    elif search_value<sorted_arr[MidIndex]:
-        LastIndex= MidIndex-1
-    else:
-        FirstIndex=MidIndex+1
-    
-if Found==False:
-    print("Not in Array")
-elif Found==True:
-    print(f"Found at index {MidIndex}")
-
+if Found==True:
+  print("Value was found at index position:", MidIndex)
+else:
+  print("Value not found")
 
 
