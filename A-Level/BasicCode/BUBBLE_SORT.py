f760b8da70d322b02866593bfb30d54bb80e471a
Samriddha Jung Pande <Samriddhajungpande@gmail.com>
2026-09-14 17:09:56 +0545

    bubble sort revised

--- A-Level/BasicCode/BUBBLE_SORT.py
+++ A-Level/BasicCode/BUBBLE_SORT.py
@@ -1,13 +1,12 @@
 
 arr=[64, 34, 25, 12, 22, 11, 90]
-boundary=len(arr)-1
-swap = True
-while swap==True and boundary>0:
-    swap=False
-    for i in range(boundary):
-        if arr[i]>arr[i+1]:
-            arr[i],arr[i+1]=arr[i+1],arr[i]
-            swap = True
-    boundary-=1
+boundary = len(arr)-1
+swap=True
 
-print("Sorted array:", arr)
+while swap==True:
+  swap=False
+  for i in range(boundary):
+    if arr[i]>arr[i+1]:
+      arr[i],arr[i+1]=arr[i+1],arr[i]
+      swap = True
+  boundary-=1
