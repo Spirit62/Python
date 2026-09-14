4f6b99a93eb5c2d4ec064ef506a7ce35342c2fdd
Samriddha Jung Pande <Samriddhajungpande@gmail.com>
2026-09-14 17:24:06 +0545

    New logic for insertion sort

--- A-Level/BasicCode/INSERTION_SORT.py
+++ A-Level/BasicCode/INSERTION_SORT.py
@@ -1,12 +1,24 @@
 arr = [100, 10, 20, 15, 30, 25, 5, 25, 40, 1]
 boundary = len(arr)
 
-for i in range(1, boundary):
-    if arr[i - 1] > arr[i]:
-        k = i
-        for j in range(i - 1, -1, -1):
-            if arr[j] > arr[k]:
-                arr[j], arr[k] = arr[k], arr[j]
-                k = k - 1
+for i in range(1,boundary):
+  if arr[i-1]>arr[i]:
+    k=i
+    for j in range(i-1,-1,-1):
+      if arr[j]>arr[k]:
+        arr[k],arr[j]=arr[j],arr[k]
+        k=k-1
 
-print("Your output:    ", arr)
+#Second method
+
+arr = [100, 10, 20, 15, 30, 25, 5, 25, 40, 1]
+
+for i in range(1,len(arr)):
+    if arr[i-1]>arr[i]:
+      key=arr[i]
+      j=i-1
+      while j>=0 and arr[j]>key:
+        arr[j+1]=arr[j]
+        j-=1
+      arr[j+1]=key
+    
